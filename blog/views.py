00eb27f19f0ed59f8c117c930d62e6acc08aa4a8
Jalilov Jafar <65230803+jafarjalilov@users.noreply.github.com>
2021-09-27 09:55:08 +0500

    Tag system & Similar posts

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 from .models import Post
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 
 # Create your views here.
-def post_list(request):
+def post_list(request, tag_slug=None):
     object__list = Post.objects.all()
+
+    # Tagging Configurations
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object__list = object__list.filter(tags__in=[tag])
+    
+    # Pagination Configurations
     paginator = Paginator(object__list, 3)
     page = request.GET.get('page')
     try:
@@ -16,12 +26,37 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
-    return render(request,'blog/list.html', {'posts': posts})
+    return render(request,'blog/list.html', {'posts': posts, 'page': page, 'tag': tag})
+
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug = post, status = 'draft', publish__year = year, publish__month = month, publish__day = day)
     
-    return render(request, 'blog/detail.html', {'post': post})
+    #List of active comments
+    comments = post.comments.filter(active=True)
+    new_comment = None
+
+    # Comment form
+    if request.method == "POST":
+        # A comment was posted
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False) # Create Comment Objet but don't save to database
+            new_comment.post = post #Assign the post to new comment
+            new_comment.save() # Save the comment to database
+        
+    else:
+        comment_form = CommentForm()
+
+    #Retrieving Posts by SIMILARITY
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.objects.all().filter(tags__in=post_tags_ids).exclude(id=post.id) # Post.published.filter instead of objects.all()filter 
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags',)[:4] # inter the '-published' order
+
+    return render(request, 'blog/detail.html', {'post': post, 'comments':comments, 'new_comment':new_comment, 'comment_form': comment_form, "similar_posts": similar_posts})
+
+
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id = post_id, status = 'draft')
--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts',
     'pages',
     'blog.apps.BlogConfig',
+    'taggit'
 ]
 
 MIDDLEWARE = [
